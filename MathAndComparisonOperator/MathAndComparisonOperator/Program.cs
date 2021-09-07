fe1c19d17f3c285852c6c79a847b1c229b77fd64
HaileeVandiver <haileevandiver@gmail.com>
2021-09-07 13:15:15 -0700

    fixed console statements

--- MathAndComparisonOperator/MathAndComparisonOperator/Program.cs
+++ MathAndComparisonOperator/MathAndComparisonOperator/Program.cs
@@ -15,21 +15,17 @@ namespace MathAndComparisonOperator
             //get data for person 1
             Console.WriteLine("Person 1");
             Console.WriteLine("What is your hourly rate?");
-            string p1Rate = Console.ReadLine();
-            int p1Hourly = Convert.ToInt32(p1Rate);
+            int p1Hourly = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many hours do you work per week?");
-            string p1Week = Console.ReadLine();
-            int p1Weekly = Convert.ToInt32(p1Week);
+            int p1Weekly = Convert.ToInt32(Console.ReadLine());
             
             //get data for person 2
             Console.WriteLine("Person 2");
             Console.WriteLine("What is your hourly rate?");
-            string p2Rate = Console.ReadLine();
-            int p2Hourly = Convert.ToInt32(p2Rate);
+            int p2Hourly = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("How many hours do you work per week?");
-            string p2Week = Console.ReadLine();
-            int p2Weekly = Convert.ToInt32(p2Week);
-            
+            int p2Weekly = Convert.ToInt32(Console.ReadLine());
+
             //multiply p1hourly and weekly to get salary
             Console.WriteLine("Annual Salary of Person 1: ");
             int p1Total = p1Hourly * p1Weekly;
@@ -40,7 +36,7 @@ namespace MathAndComparisonOperator
             int p2Total = p2Hourly * p2Weekly;
             Console.WriteLine(p2Total);
 
-            //create boolean for if person 1 has larger salary or not
+            //create boolean for if person 1 has larger sala
             Console.WriteLine("Does Person 1 make more than Person 2");
             bool biggerSalary = p1Total > p2Total;
             Console.WriteLine(biggerSalary);
