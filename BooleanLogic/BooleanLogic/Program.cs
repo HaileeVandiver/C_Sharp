ee1b5330b38687aaca9be22cbca65f9421bfb6a6
HaileeVandiver <haileevandiver@gmail.com>
2021-09-07 13:47:17 -0700

    boolean logic update

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int yourAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualified = (yourAge > 15 && !DUI && tickets < 3);
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+          
+
+
+
+
+        }
+    }
+}
