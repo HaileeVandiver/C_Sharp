7e5397ded804e6366645b5e2692f3217bf87f634
HaileeVandiver <haileevandiver@gmail.com>
2021-09-12 15:10:16 -0700

    try catch block

--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -4,12 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExceptionHandling
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Create a list of integers
+        List<int> myList = new List<int>
         {
+            1,
+            2,
+            3,
+            500,
+            666,
+            321
+        };
+        //ask the user for a number to divide each number in the list by
+        Console.WriteLine("pick a number to divide each number in this magical list by");
+        int userNum = Convert.ToInt32(Console.ReadLine());
+
+        for (int i =0; i <myList.Count; i++)
+        {
+            try {
+                Console.WriteLine(myList[i] / userNum);
+                Console.ReadLine();
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Please type a whole number");
+                return;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                
+                Console.ReadLine();
+            }
         }
+        Console.WriteLine("We have emerged froom the try catch block");
+
     }
 }
+
