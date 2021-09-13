893c36556ab26f6512837182c0c6a2481424f1ff
HaileeVandiver <haileevandiver@gmail.com>
2021-09-13 11:38:34 -0700

    main method

---
+++ MainMethod/MainMethod/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+   public class Math
+    {
+        public int mathOps(int number)
+        {
+            return number * 100; 
+        }
+
+        //method overload to accept decimal 
+        public int mathOps(decimal number)
+        {
+            int newNum = Convert.ToInt32(number);
+            return newNum + 2;
+            
+        }
+
+        //method overload to accept a string 
+        public int mathOps(string number)
+        {
+            int newNum = Convert.ToInt32(number);
+            return newNum / 2; 
+        }
+
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate object of Math class
+            Math firstOp = new Math(); 
+           
+            Console.WriteLine("Please enter a number");
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number * 100 is: "+ firstOp.mathOps(inputNum));
+            Console.ReadLine();
+
+            //instantiate new object using second method 
+            Math secondOp = new Math();
+
+            Console.WriteLine("Please enter a decimal");
+            decimal floatNum = Decimal.Parse(Console.ReadLine()); 
+            Console.WriteLine(" Your number + 2 is: " + secondOp.mathOps(floatNum));
+            Console.ReadLine();
+
+            //instantiate new object using third method 
+            Math thirdOp = new Math();
+            Console.WriteLine("Please enter a number");
+            Console.WriteLine("Your number divided by 2 is: " + thirdOp.mathOps(Console.ReadLine()));
+            Console.ReadLine(); 
+        }
+    }
+}
