f60147648290d58e4907a96599d36a2fb5225a44
HaileeVandiver <haileevandiver@gmail.com>
2021-09-13 12:35:04 -0700

    methods

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class
+            Math firstOp = new Math();
+
+            int numOne = 10;
+            int numTwo = 6;
+
+            //call method and pass in two numbers 
+
+            firstOp.mathOp(numOne, numTwo);
+
+            // call the method in the class and specify parameters by name 
+            firstOp.mathOp(secondNum: numTwo, firstNum: numOne);
+            Console.ReadLine(); 
+
+
+
+
+        }
+    }
+}
