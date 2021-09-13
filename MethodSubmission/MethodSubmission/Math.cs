edad573cdaf70b8a2c1317b23d6d2801f555d39b
HaileeVandiver <haileevandiver@gmail.com>
2021-09-13 12:04:47 -0700

    method submission

---
+++ MethodSubmission/MethodSubmission/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class Math
+    {
+        public int mathOps(int number, int numberTwo = 0)
+        {
+            int resultNum = number + 3 + numberTwo; 
+            return resultNum; 
+            
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //isntantiate a class
+            Math firstOp = new Math();
+
+            //takes int as a parameter 
+            Console.WriteLine("Please enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            //optional parameter
+            Console.WriteLine("Please enter another number, if you feel like it, you don't have to");
+            bool numberTwoValid = int.TryParse(Console.ReadLine(), out int numberTwo);
+
+            //two numbers entered 
+
+            int result = 0;
+            if (numberTwoValid)
+            {
+                result = firstOp.mathOps(number, numberTwo);
+            }
+            else
+            {
+                result = firstOp.mathOps(number);
+            }
+            
+            Console.WriteLine("Your result is: " + result);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
