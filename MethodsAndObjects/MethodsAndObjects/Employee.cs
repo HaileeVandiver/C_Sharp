fca769498944f2ab1aaf6784995cb7a751fcc5ad
HaileeVandiver <haileevandiver@gmail.com>
2021-09-13 20:51:38 -0700

    update

---
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    //extension of person class
+    public class Employee : Person
+    {
+        //properties
+        public int Id { get; set; }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person
+    {
+        //class properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //void method because no arguments are passed through
+        public void SayName()
+        {
+            //prints names to console
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+            Console.ReadLine(); 
+        }
+    }
+}
