c1c36fc54512005a9834d56ddedc3f05a5bfc5d3
HaileeVandiver <haileevandiver@gmail.com>
2021-09-13 20:11:27 -0700

    class submission

---
+++ ClassSubmission/ClassSubmission/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Math
+    {
+        //void: division operation
+        public void myMethodOp(int userNum)
+        {
+            Console.WriteLine("Your number divided by two is: " + (userNum / 2));
+        }
+
+        //overload method and static 
+
+        public static int myMethodOp(int userNum, int numTwo)
+        {
+            int resultNum = userNum * numTwo;
+            return resultNum;
+        }
+
+        
+        //using out parameter 
+        public void getValue(out int x)
+        {
+            int temp = 5;
+            x = temp;
+
+
+        }
+    
+    }
+}
--- ClassSubmission/ClassSubmission/Program.cs
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -10,6 +10,39 @@ namespace ClassSubmission
     {
         static void Main(string[] args)
         {
+           
+
+            //instantiate MathOp class
+            Math myNumObj = new Math();
+
+            //user input is divided by 2 and output is displayed
+            Console.WriteLine("Input a numba: ");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            myNumObj.myMethodOp(userNum);
+
+            // method with an out parameter 
+    
+            int a;
+            myNumObj.getValue(out a);
+            Console.WriteLine("After method call, value of a : {0}", a);
+
+            //static overload method 
+
+
+            int numTwo = 2;
+            Console.WriteLine("Your number times 2 is: " + Math.myMethodOp(userNum, numTwo));
+
+            
+            
+
+
+
+
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
