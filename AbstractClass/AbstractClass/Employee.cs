a068e0c419bef5978c459c352c4beff7d8be01ed
HaileeVandiver <haileevandiver@gmail.com>
2021-09-14 12:00:51 -0700

    added overload

--- AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/Employee.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace AbstractClass
 {
     public class Employee : Person, IQuittable
+
     {
+        public int employeeID { get; set; }
         public override void SayName()
         {
             Console.WriteLine("Name: {0} {1}", firstName, lastName);
@@ -17,6 +19,34 @@ namespace AbstractClass
         public void Quit()
         {
             Console.WriteLine("This employee quit");
+        }
+        public static bool operator ==(Employee one, Employee two)
+        {
+            if (one.employeeID == two.employeeID)
+            {
+                Console.WriteLine("The ID's are equal");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("The ID's are not equal.");
+                return false;
+            }
+
+        }
+
+        public static bool operator !=(Employee one, Employee two)
+        {
+            if (one.employeeID == two.employeeID)
+            {
+                Console.WriteLine("The ID's are equal");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("The ID's are not equal.");
+                return true;
+            }
         }
     }
 }
--- AbstractClass/AbstractClass/Program.cs
+++ AbstractClass/AbstractClass/Program.cs
@@ -10,18 +10,31 @@ namespace AbstractClass
     {
         static void Main(string[] args)
         {
-            //create object 
-            Employee empObject = new Employee();
-            empObject.firstName = "Sample";
-            empObject.lastName = "Student";
+            //create object
+            //Employee empObject = new Employee();
+            //empObject.firstName = "Sample";
+            //empObject.lastName = "Student";
 
-        
+
             //call method
-            empObject.SayName();
+            //empObject.SayName();
 
-            //use polymorphism to create an object of IQuittable and call Quit() 
-            IQuittable empQuit = new Employee();
-            empQuit.Quit(); 
+            //use polymorphism to create an object of IQuittable and call Quit()
+            //IQuittable empQuit = new Employee();
+            //empQuit.Quit();
+
+            Employee employeeOne = new Employee();
+            employeeOne.firstName = "Hailee";
+            employeeOne.lastName = "Vandiver";
+            employeeOne.employeeID = 1234;
+
+
+            Employee employeeTwo = new Employee();
+            employeeTwo.firstName = "Tami";
+            employeeTwo.lastName = "Lenee";
+            employeeTwo.employeeID = 2345;
+
+            bool doesEqual = employeeTwo == employeeOne; 
 
             Console.ReadLine(); 
         }
