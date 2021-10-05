590970dbcbe4d695e2a7fdd3deaff07d7f3ecf91
HaileeVandiver <haileevandiver@gmail.com>
2021-10-05 11:23:22 -0700

    final challenge

---
+++ EntityFrameworkApp/EntityFrameworkApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EntityFrameworkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Hailee" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
